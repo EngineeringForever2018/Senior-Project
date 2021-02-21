721aabe4abdf08fc3c61baca558704a6775bbcff
grchristensen <grchr4000@gmail.com>
2021-02-21 14:50:50 -0800

    Add GroupSentences pipeline stage

--- notebooks/notebooks/pipes.py
+++ notebooks/notebooks/pipes.py
@@ -1,3 +1,9 @@
+from itertools import chain, tee, repeat
+
+from tqdm import tqdm
+
+from notebooks.utils import split_into_sentences
+import pandas as pd
 from pdpipe import PdPipelineStage
 
 
@@ -8,8 +14,82 @@ class SplitText(PdPipelineStage):
         self.nlp = nlp
         self.show_loading = show_loading
 
-    def _prec(self, df): # noqa
+    def _prec(self, df):  # noqa
         return 'text' in df.columns
 
     def _transform(self, df, verbose):
-        pass
+        authors = []
+        text_ids = []
+        sentence_positions = []
+        sentences = []
+
+        if self.show_loading:
+            rows = tqdm(df.itertuples())
+        else:
+            rows = df.itertuples()
+
+        for row in rows:
+            row_authors, row_text_ids, row_sentence_positions, row_sentences = self._split_row(row)
+
+            authors.append(row_authors)
+            text_ids.append(row_text_ids)
+            sentence_positions.append(row_sentence_positions)
+            sentences.append(row_sentences)
+
+        authors = chain(*authors)
+        text_ids = chain(*text_ids)
+        sentence_positions = chain(*sentence_positions)
+        sentences = chain(*sentences)
+
+        col_dict = {'author': list(authors), 'text_id': list(text_ids), 'sentence_position': list(sentence_positions),
+                    'sentence': list(sentences)}
+
+        return pd.DataFrame(col_dict)
+
+    def _split_row(self, row):
+        sentences = split_into_sentences(row.text, nlp=self.nlp)
+        sentences, sentences_copy = tee(sentences)
+        sentence_count = len(list(sentences_copy))
+
+        authors = repeat(row.author, times=sentence_count)
+        text_ids = repeat(row.Index, times=sentence_count)
+        sentence_positions = (position for position, _ in enumerate(range(sentence_count)))
+
+        return authors, text_ids, sentence_positions, sentences
+
+
+class GroupSentences(PdPipelineStage):
+    def __init__(self, n):
+        desc = 'A pipeline that will group sentences from a dataframe into ordered groups of n.'
+        super().__init__(desc=desc)
+        self.n = n
+
+    def _prec(self, df): # noqa
+        return True
+
+    def _transform(self, df, verbose):
+        def group_sentences(text_group):
+            sentence_positions = text_group['sentence_position']
+            result = text_group.copy()
+
+            # Just integer divide by the group length. If the sentence positions are [0, 1, 2, 3], and the group length
+            # is 2, then the resulting group positions would be [0, 0, 1, 1], which is desirable.
+            result['group_position'] = (sentence_positions / self.n).astype(int)
+            result['sentence_position'] -= result['group_position'] * self.n
+
+            last_sentence_position = max(sentence_positions)
+            remainder = (last_sentence_position + 1) % self.n
+            max_sentence_position = last_sentence_position - remainder
+
+            # Wherever the sentence position is greater than the max sentence position, we need to replace group with
+            # -1 (which means we're going to drop those rows later).
+            result['group_position'][sentence_positions > max_sentence_position] = -1
+
+            return result
+
+        df = df.groupby('text_id').apply(group_sentences)
+
+        df = df[df['group_position'] != -1]
+
+        return df.reset_index(drop=True)
+
--- notebooks/notebooks/test/pipes.py
+++ notebooks/notebooks/test/pipes.py
@@ -30,13 +30,23 @@ class TestSplitText:
         ], columns=['author', 'text', 'flavor'])
     ]
 
+    # TODO: Have pipeline keep other columns
+    # expected_dataframes = [
+    #     pd.DataFrame([
+    #         [5, 0, 0, 'I am sentence 1.', 'blueberry'],
+    #         [5, 0, 1, 'I am sentence 2.', 'blueberry'],
+    #         [7, 1, 0, 'I am sentence 1.', 'cherry'],
+    #         [7, 1, 1, 'I am sentence 2.', 'cherry']
+    #     ], columns=['author', 'text_id', 'sentence_position', 'sentence', 'flavor'])
+    # ]
+
     expected_dataframes = [
         pd.DataFrame([
-            [5, 0, 0, 'I am sentence 1.', 'blueberry'],
-            [5, 0, 1, 'I am sentence 2.', 'blueberry'],
-            [7, 1, 0, 'I am sentence 1.', 'cherry'],
-            [7, 1, 1, 'I am sentence 2.', 'cherry']
-        ], columns=['author', 'text_id', 'sentence_position', 'sentence', 'flavor'])
+            [5, 0, 0, 'I am sentence 1.'],
+            [5, 0, 1, 'I am sentence 2.'],
+            [7, 1, 0, 'I am sentence 1.'],
+            [7, 1, 1, 'I am sentence 2.']
+        ], columns=['author', 'text_id', 'sentence_position', 'sentence'])
     ]
 
     @pytest.mark.parametrize('df, expected_result', zip(dataframes, expected_dataframes))
@@ -46,3 +56,44 @@ class TestSplitText:
         result = pipeline(df)
 
         assert result.equals(expected_result)
+
+
+class TestGroupSentences:
+    dataframes = [
+        pd.DataFrame([
+            [5, 0, 0, 'I am sentence 1.'],
+            [5, 0, 1, 'I am sentence 2.'],
+            [5, 0, 2, 'I am sentence 3.'],
+            [5, 1, 0, 'I am sentence 1.'],
+            [5, 1, 1, 'I am sentence 2.'],
+            [5, 1, 2, 'I am sentence 3.'],
+            [5, 1, 3, 'I am sentence 4.'],
+            [7, 2, 0, 'I am sentence 1.'],
+            [7, 2, 1, 'I am sentence 2.'],
+            [7, 2, 2, 'I am sentence 3.'],
+            [7, 2, 3, 'I am sentence 4.']
+        ], columns=['author', 'text_id', 'sentence_position', 'sentence'])
+    ]
+
+    expected_dataframes = [
+        pd.DataFrame([
+            [5, 0, 0, 0, 'I am sentence 1.'],
+            [5, 0, 0, 1, 'I am sentence 2.'],
+            [5, 1, 0, 0, 'I am sentence 1.'],
+            [5, 1, 0, 1, 'I am sentence 2.'],
+            [5, 1, 1, 0, 'I am sentence 3.'],
+            [5, 1, 1, 1, 'I am sentence 4.'],
+            [7, 2, 0, 0, 'I am sentence 1.'],
+            [7, 2, 0, 1, 'I am sentence 2.'],
+            [7, 2, 1, 0, 'I am sentence 3.'],
+            [7, 2, 1, 1, 'I am sentence 4.']
+        ], columns=['author', 'text_id', 'group_position', 'sentence_position', 'sentence'])
+    ]
+
+    @pytest.mark.parametrize('df, expected_result', zip(dataframes, expected_dataframes))
+    def test_pipe_groups_into_two(self, df, expected_result):
+        pipeline = pipes.GroupSentences(n=2)
+
+        result = pipeline(df)
+
+        assert result.eq(expected_result).all().all()
