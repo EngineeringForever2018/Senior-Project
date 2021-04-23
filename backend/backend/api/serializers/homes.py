65b8711b5898b9a703770a60072ac47f384a02be
EngineeringForever2018 <jaredlam@nevada.unr.edu>
2021-04-23 15:59:46 -0700

    We added student home page
    Co-authored-by: grchristensen <grchr4000@gmail.com>

--- backend/backend/api/serializers/homes.py
+++ backend/backend/api/serializers/homes.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from rest_framework.serializers import HyperlinkedModelSerializer
 from backend.api.models import Student, Classroom, Assignment
+from backend.api.serializers import AssignmentSerializer
 
 
-class StudentHomeSerializer(HyperlinkedModelSerializer):
+class StudentHomeSerializer(serializers.ModelSerializer):
+    #assignments = serializers.PrimaryKeyRelatedField(source = 'classrooms.assignments', queryset = Assignment.objects.all())
+    assignments = serializers.SerializerMethodField()
     class Meta:
         model = Student
-        fields = [ "classrooms", "assignments" ]
+        fields = ['classrooms', 'assignments', 'id']
+    def get_assignments(self,obj):
+        queryassignments = Assignment.objects.filter(classroom__in = obj.classrooms.all())
+        return AssignmentSerializer(queryassignments, many = True).data
