4413248945dd1a1550f5210aa8d6fa8e7992bb8d
Gage Christensen <grchr4000@gmail.com>
2021-05-01 23:41:40 -0700

    even more debug output

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -1,3 +1,5 @@
+pwd
+ls -a
 source .env.local
 ./cloud_sql_proxy -credential_file=service-key.json -instances=$SQL_CONNECTION_NAME=tcp:5432 &
 python manage.py migrate --settings=backend.settings.production
